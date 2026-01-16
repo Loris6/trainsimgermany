5727270d427788cded712f7c0f52a6d9b438bb2f
Loris6 <lorismziray01@gmail.com>
2026-01-16 05:49:26 +0100

    update

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "tsg-cache-v1";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+/* Install */
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("✅ Cache ready");
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+/* Activate */
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log("🧹 Old cache removed:", key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+/* Fetch */
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
